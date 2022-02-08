a264218da38f8adba717d5209890b5d1c068dcd1
Myoung Ji Kim <66910643+myoungji-kim@users.noreply.github.com>
2022-02-09 00:19:41 +0900

    Add files via upload

--- Servlet/1차 풀이/servlet_workshop10/src/com/controller/LoginCheckServlet.java
+++ Servlet/1차 풀이/servlet_workshop10/src/com/controller/LoginCheckServlet.java
@@ -22,7 +22,7 @@ public class LoginCheckServlet extends HttpServlet {
 		
 		response.setContentType("text/html;charset=UTF-8");
 		PrintWriter out = response.getWriter();
-		out.print(result);
+		out.print(result+"우에에엑꿹");
 
 	}
 	
