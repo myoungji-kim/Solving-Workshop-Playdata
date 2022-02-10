0cb7b0f336a883dfeded49f36e11d70cb8563d57
Myoung Ji Kim <66910643+myoungji-kim@users.noreply.github.com>
2022-02-11 00:02:29 +0900

    Add files via upload

---
+++ Servlet/1차 풀이/servlet_workshop10_ajax/src/com/controller/LoginCheckServlet.java
@@ -0,0 +1,34 @@
+package com.controller;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+@WebServlet("/loginCheckServlet")
+public class LoginCheckServlet extends HttpServlet {
+
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		request.setCharacterEncoding("UTF-8");
+		String id = request.getParameter("id");
+		String result = "사용 가능";
+		if("test".equals(id)) {
+			result = "사용 불가";
+		}
+		
+		response.setContentType("text/html;charset=UTF-8");
+		PrintWriter out = response.getWriter();
+		out.print(result+"우에에엑꿹");
+
+	}
+	
+	protected void doPost(HttpServletRequest request, HttpServletResponse response)
+			throws ServletException, IOException {
+		doGet(request, response);
+	}
+
+}
